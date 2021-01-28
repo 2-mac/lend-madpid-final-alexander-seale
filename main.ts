9dc3cd857f76deb70ede9de930cbdcea67e5ff0a
2-mac <77454800+2-mac@users.noreply.github.com>
2021-01-28 16:43:57 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,27 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 2 2 2 . . . . . . . . . 
+        . . . . . . . . . . 2 2 . . . . 
+        . . 2 2 4 4 4 4 4 . 2 2 . . . . 
+        . . 2 2 4 4 4 5 4 4 . . . . . . 
+        . . . 2 4 4 4 5 5 4 4 . . . . . 
+        . . . . 4 5 5 5 5 4 4 . . . . . 
+        . . . 4 4 4 4 5 5 4 . . . . . . 
+        . 2 . 4 4 . 4 4 5 4 . . . . . . 
+        . 2 . . . 4 . 4 4 4 2 2 . . . . 
+        . . . . 4 4 . . . . . 2 . . . . 
+        . . . . . . . 2 . . . . . . . . 
+        . . . . . . . 2 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 100, 0)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
 scene.setBackgroundImage(assets.image`Tester`)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . . d d d d . . . . . . 
     . . . . . . f d d f . . . . . . 
     . . . . . . d d d d . . . . . . 
@@ -18,24 +40,6 @@ let mySprite = sprites.create(img`
     . . . 5 5 5 5 . . 5 5 5 5 . . . 
     `, SpriteKind.Player)
 mySprite.setVelocity(0, 0)
-let projectile = sprites.createProjectileFromSprite(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . 2 2 2 . . . . . . . . . 
-    . . . . . . . . . . 2 2 . . . . 
-    . . 2 2 4 4 4 4 4 . 2 2 . . . . 
-    . . 2 2 4 4 4 5 4 4 . . . . . . 
-    . . . 2 4 4 4 5 5 4 4 . . . . . 
-    . . . . 4 5 5 5 5 4 4 . . . . . 
-    . . . 4 4 4 4 5 5 4 . . . . . . 
-    . 2 . 4 4 . 4 4 5 4 . . . . . . 
-    . 2 . . . 4 . 4 4 4 2 2 . . . . 
-    . . . . 4 4 . . . . . 2 . . . . 
-    . . . . . . . 2 . . . . . . . . 
-    . . . . . . . 2 . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, mySprite, 50, 50)
 controller.moveSprite(mySprite)
 let mySprite2 = sprites.create(img`
     . 4 4 5 5 5 5 f f f f f f 5 5 . 
